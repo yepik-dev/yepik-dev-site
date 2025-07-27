7abc6e8b28affa6636c5d7fd3e65c75be7a9e22b
Andy Yepik <98517929+yepik-dev@users.noreply.github.com>
2025-07-27 08:32:58 +0200

    chore: install @strapi/client v1.4.0
    Added Strapi JS SDK for interacting with public API
    as a non-authenticated user (e.g., subscriptions, contact forms).
    Configured public API token for read-only and message/email creation.

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { strapi } from "@strapi/client";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const client = strapi({
+  baseURL: process.env.NEXT_PUBLIC_STRAPI_URL || 'http://127.0.0.1:1337/api',
+  auth: process.env.NEXT_PUBLIC_STRAPI_TOKEN,
+
+});

6b77b3e84dbf1d0202c0d73c6df8903ad84f20a1
Andy Yepik <98517929+yepik-dev@users.noreply.github.com>
2025-07-29 20:23:26 +0200

    feat: returned to simple fetch request

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,13 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { strapi } from "@strapi/client";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const client = strapi({
-  baseURL: process.env.NEXT_PUBLIC_STRAPI_URL || 'http://127.0.0.1:1337/api',
-  auth: process.env.NEXT_PUBLIC_STRAPI_TOKEN,
-
-});
